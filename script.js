0c4ec3fa69c9ecfbb84167807f62ffffe15fe8f2
juan diaz <125918128+relampajo@users.noreply.github.com>
2023-04-27 17:41:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function displaytime() {
+    var datetime = new Date();
+    var hrs = datetime.getHours();
+    var min = datetime.getMinutes();
+    var sec = datetime.getSeconds();
+    var session = document.getElementById('session');
+
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+
+    if(hrs > 12){
+        hrs = hrs - 12;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displaytime, 10);
